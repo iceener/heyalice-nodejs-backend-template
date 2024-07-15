34b8777b63a53b981748c24af7795bdbd9eb2aca
Adam Gospodarczyk <adam@overment.com>
2024-07-15 10:49:46 +0200

    Added non-streaming response

--- controllers/chat.ts
+++ controllers/chat.ts
@@ -5,22 +5,22 @@ import {getCustomSystemMessage, context} from '../context/messages.ts';
 export const parseChatRequest = async (req: Request) => {
     const stream = req.body.stream;
     const messages = req.body.messages;
-    messages.shift(); // Remove Alice's app system message.
 
     /*
-     Prepare chat messages for the OpenAI API
+     Domyślnie aplikacja Alice przesyła wiadomość systemową. Możesz ją jednak usunąć, jeśli chcesz.
      */
-    const formattedMessages = [
-        ...(await getCustomSystemMessage(context)),
-        ...messages,
-    ];
+    // messages?.shift(); // Remove Alice's app system message.
+    // const formattedMessages = [
+    //     ...(await getCustomSystemMessage(context)),
+    //     ...messages,
+    // ];
 
-    return { stream, messages: formattedMessages };
+    return { stream, messages: messages };
 };
 
 export const chatResponse = async (answer: string | ReadableStream<string>, stream: boolean, res: Response) => {
     if (!stream || typeof answer === 'string') {
-        res.json(answer);
+        return res.json(answer);
     } else {
         res.writeHead(200, {
             'Content-Type': 'application/x-ndjson',
--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import { chatEndpoint } from './routes/chat';
 import { initializeOpenAI } from './providers/llm.ts';
+import bodyParser from "body-parser";
 
 const initializeServer = () => {
     const app = express();
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
     app.use(express.json());
     return app;
 };
@@ -13,7 +16,7 @@ const openai = initializeOpenAI();
 
 app.post('/api/chat', chatEndpoint(openai));
 
-const port = process.env.PORT || 3005;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3005;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
